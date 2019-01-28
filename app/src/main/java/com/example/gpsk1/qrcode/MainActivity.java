0f7fa7969208dbee28851d6ef5f28b67958e78c2
Hyenakim <gpsk1202@gmail.com>
2019-01-28 18:31:45 +0900

    알람창 (뜨기전에 넘어가버림)

--- app/src/main/java/com/example/gpsk1/qrcode/MainActivity.java
+++ app/src/main/java/com/example/gpsk1/qrcode/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.gpsk1.qrcode;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.nfc.Tag;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
@@ -16,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button start;
     private static final String TAG = "MainActivity";
+    private AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
         integrator.setCaptureActivity(CustomScannerActivity.class);
         integrator.initiateScan();
     }
@@ -34,12 +41,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Log.e(TAG, ">>> requestCode = " + requestCode + ", resultCode = " + resultCode);
-
-        if(requestCode == IntentIntegrator.REQUEST_CODE){
+        if(resultCode == Activity.RESULT_OK){
+        //if(requestCode == IntentIntegrator.REQUEST_CODE){
             IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+            final String url = result.getContents();
             Log.i(TAG, ">>> result.getContents()   :  " + result.getContents());
             Log.i(TAG, ">>> result.getFormatName()   :  " + result.getFormatName());
 
+            //Toast.makeText(this, url, Toast.LENGTH_LONG).show();
+            //알람창 안됨
+            builder = new AlertDialog.Builder(this);
+            builder.setMessage(result.getContents()+"로 이동하시겠습니까?");
+            builder.setPositiveButton("예",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                            intent.setPackage("com.android.chrome");
+                            startActivity(intent);
+                        }
+                    });
+            builder.setNegativeButton("아니오",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+            builder.show();
         }
+
+
     }
 }
